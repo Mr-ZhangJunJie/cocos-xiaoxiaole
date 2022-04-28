6441ac0d4b5d26c5b2b7e2d7059e31194469c2fa
zhang.junjie <zhang.junjie@immomo.com>
2022-04-28 11:30:30 +0800

    done animation

--- assets/Scripts/Controllers/GameController.ts
+++ assets/Scripts/Controllers/GameController.ts
@@ -5,6 +5,11 @@ import GameModel from '../Model/GameModel'
 import { ContainerView } from '../Views/ContainerView'
 import { EFECT_TYPE } from '../Types/index'
 
+// ajax
+// var xhr = new XMLHttpRequest()
+// fetch()
+//
+
 /**
  * Predefined variables
  * Name = GameController
@@ -31,6 +36,9 @@ export class GameController extends Component {
   containerView: ContainerView = null
 
   onLoad() {
+    // @ts-ignore  页面加载完成后调用
+    // pageLoad()
+
     // 初始化游戏数据模型
     this.gameMode = new GameModel()
     // 初始化4种类型，并且mock 9 * 9 且不存在 连续3个以上的单元格 数据
@@ -39,6 +47,17 @@ export class GameController extends Component {
     this.containerView.setController(this)
     // 初始化单元格视图，并且指定位置
     this.containerView.initWithCellModels(this.gameMode.getCells())
+
+    // const instance = axios.create({
+    //   baseURL: 'http://live-web-api.staging2.p1staff.com',
+    //   timeout: 5000
+    // })
+
+    // instance({
+    //   method: 'get',
+    //   url: '/v2/campaigns/leaderboard-records',
+    //   params: {}
+    // })
   }
 
   selectCell(pos: Vec2): [CellModel[], EFECT_TYPE[]] {
--- extensions/vue3_temp/dist/main.js
+++
@@ -1,3 +0,0 @@
-exports.load = function(){
-	console.warn("vue3_temp is not compiled yet.")
-}
